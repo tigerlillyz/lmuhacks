9deedc409d48b83d342551d17b3b3e8d47c3fef9
tigerlillyz <tigerlillyzietz@gmail.com>
2020-02-01 19:31:18 -0800

    linked

--- screen.js
+++ screen.js
@@ -1,20 +1,34 @@
-let blackList = ["retard", "bitch", "cunt"];
+let mockTweets = [ "What happens when a Survivalist and Super Star go on a jungle adventure? Watch @BearGrylls and @captainmarvel @brielarson navigate croc-filled mangroves and rappel sheer cliffs on Running Wild with Bear Grylls on National Geographic, Mon-Wed at 10 PM.",
+"Little moments like this remind me why I love my job. This is on the banks of a river in southern India, filming some elephants and scouting some amazing locations. (Then stripping down and bathing in the river, cooling off the sweat and dust, and soaking in the sun!",
+"You did amazingly! Huge respect. Courageous, determined, humble, fun. A winning combo! People will love our adventure I am sure.",
+"Wow i hate the outdoors. I'm tired of this job.",
+"Honestly just screw the turtles. #PlasticDrawsLiveOn.",
+"I might kill this camera man hes such a loser."
 
-let tweet = "@josh is such a retard";
+];
+  let blacklist = ["hate", "screw", "kill"];
+  let whitelist = ["super", "love", "respect"];
 
-let badTweets = [];
+  let flaggedResult = [];
+  let whitelistResult = [];
+  let flaggedOutput = document.getElementById("flagged");
+  let whitelistOutput = document.getElementById("interest");
+  function getTwitterFeed(){
+    return mockTweets;
+  }
+  function filterTweets() {
+    for (i = 0; i < getTwitterFeed().length; i++) {
+      for (j = 0; j < blacklist.length; j++) {
+        if (getTwitterFeed()[i].includes(blacklist[j])) {
+            flaggedResult[i] = getTwitterFeed()[i];
+        } else if (getTwitterFeed()[i].includes(whitelist[j])) {
+            whitelistResult[i] = getTwitterFeed()[i];
 
-
-
-function checkTweet(list, tweet){
-    for(let i = 0; i < list.length; i++){
-        if(tweet.includes(list[i])){
-            badTweets.push(tweet);
         }
-        
+      }
     }
-}
+  }
 
-console.log(getTweet());
-console.log(checkTweet(blackList, tweet));
-console.log(badTweets);
+  filterTweets();
+(flaggedResult.filter(tweet => tweet != null)).forEach(tweet => flaggedOutput.innerHTML += tweet + "<br> <br>");
+(whitelistResult.filter(tweet => tweet != null)).forEach(tweet => whitelistOutput.innerHTML += tweet + "<br> <br>");
