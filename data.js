f5e7eefdbeccee5d82a43d320e8af2dd671cdd48
tigerlillyz <tigerlillyzietz@gmail.com>
2020-02-01 16:47:13 -0800

    mock data

---
+++ data.js
@@ -0,0 +1,37 @@
+data = {
+    1: {
+        "username": "realDonaldTrump",
+        "tweet": [
+            "Trump poll numbers are the highest since election, despite constant phony Witch Hunts! Tens of thousands of people attending rallies (which the Fake News never mentions) to see The Greatest Show On Earth”. Fun because USA is WINNING AGAIN!",
+             "The Radical Left, Do Nothing Democrats keep chanting “fairness”, when they put on the most unfair Witch Hunt in the history of the U.S. Congress. They had 17 Witnesses, we were allowed ZERO, and no lawyers. They didn’t do their job, had no case. The Dems are scamming America!",
+             "To keep America Safe, we have fully rebuilt the U.S. Military – it is now stronger, more powerful, and more lethal than ever before. Thanks to the courage of American Heroes, the ISIS Caliphate has been DESTROYED & its founder & leader – the animal known as al-Baghdadi – is DEAD!"
+            ]
+    },
+
+    2: {
+        "username": "elonmusk",
+        "tweet": [
+            "I wrote the lyrics & performed the vocals!!",
+            "Latest weather data suggests sustained winds and rough seas in the recovery area during the top of tomorrow’s four-hour launch escape test window; now targeting toward the end of the window. Will continue to monitor weather and update the T-0 accordingly in the morning",
+            "LIVE NOW: Hear from mission experts as they talk about @SpaceX's In-Flight Abort Test. The test will demonstrate #CrewDragon’s ability to safely escape the Falcon 9 rocket in the unlikely event of an emergency during launch."
+        ]
+    },
+
+    3: {
+        "username": "BearGrylls",
+        "tweet": [
+            "What happens when a Survivalist and Super Star go on a jungle adventure? Watch @BearGrylls and @captainmarvel @brielarson navigate croc-filled mangroves and rappel sheer cliffs on Running Wild with Bear Grylls on National Geographic, Mon-Wed at 10 PM.",
+            "Little moments like this remind me why I love my job. This is on the banks of a river in southern India, filming some elephants and scouting some amazing locations. (Then stripping down and bathing in the river, cooling off the sweat and dust, and soakin…",
+            "You did amazingly! Huge respect. Courageous, determined, humble, fun. A winning combo! People will love our adventure I am sure"
+        ]
+    },
+
+    4: {
+        "username": "rtoal",
+        "tweet": [
+            "Wait Java does not have raw strings? Okay I guess \\\\\\\\ it is",
+            "Languages turning 25 this year: JavaScript (written in May 1995, named JS in December) Java (released by Sun in 1995) Ruby (first public release 1995-12-21) PHP (Unsenet announcement 1995-06-08) Racket Python is older, first released in 1991. C from 1972 and Lisp late 1950s.",
+            "When did people start using the words 'codes' for programming? As in 'I have Python codes' or 'My C++ codes'? That sounds really odd and yet I think it is becoming pretty common at least on StackOverflow. Anyone know the etymology?"
+        ]
+    }
+}
